3a1fbee288b1b01657e20191c80f7614b38de39b
Prudhvi <prudhvisoft92@gmail.com>
2022-12-28 23:34:36 +0530

    completed Basic1 and datatypes

---
+++ CSharp/BasicsTraining/MyDatatypes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsTraining
+{
+    public class MyDatatypes
+    {
+        
+        //Data members or fields
+        public int x = 10;
+        public int y = 20;
+
+        //Data function or methods 
+        public void printMyDataType()
+        {
+            Console.WriteLine(x+y);
+        }
+        //access specifies
+        /*
+          public
+            private
+                internal
+                    protected
+                        protected internal
+         */
+    }
+}
